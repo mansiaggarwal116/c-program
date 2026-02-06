452fd5eec027d4c7fa04325f66f91af50120cb19
mansi aggarwal <mansiaggarwal116@gmail.com>
2026-02-06 18:31:42 +0530

    stack

--- sortingarray.cpp
+++ sortingarray.cpp
@@ -5,7 +5,7 @@ int main(){
     for(int i=0; i<5; i++){
         if(arr[i] < arr[i+1]){
             cout<<"array is sorted in ascending order"<<endl;
-            continue;
+            
          }
         else if(arr[i] > arr[i+1]){
             cout<<"array is not sorted in ascending order"<<endl;
